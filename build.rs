68cea580a4d18b2fca3d2063619108229345f05c
jjayrex <jjayrex@gmail.com>
2025-10-29 02:48:52 +0100

    Initial commit

---
+++ build.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut res = winres::WindowsResource::new();
+    res.set("FileDescription", "A multi-functional toolkit");
+    res.set("ProductName", "JJ-Toolkit");
+    res.set("LegalCopyright", "JJayRex");
+    res.set("FileVersion", "0.1.0.0");
+    res.set("ProductVersion", "0.1.0.0");
+    res.compile().unwrap();
+}
